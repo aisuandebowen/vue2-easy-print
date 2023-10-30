393cbb2a38cc4e82ac5fb984ea51ab4c28fd4d0c
Boen Chen <58322181+aisuandebowen@users.noreply.github.com>
2023-10-30 17:21:07 +0800

    Update utils.js

--- src/print/utils.js
+++ src/print/utils.js
@@ -1,3 +1,9 @@
+/**
+ * 复制样式
+ * @param {DOM} sourceEl 源DOM
+ * @param {DOM} targetEl 目标DOM
+ * @param {Boolean} isDeep 是否深度复制（包含孩子）
+ */
 export function copyStyle(sourceEl, targetEl, isDeep = true) {
   const sourceStyle = window.getComputedStyle(sourceEl); // 获取样式
   // 复制样式
@@ -15,4 +21,15 @@ export function copyStyle(sourceEl, targetEl, isDeep = true) {
       copyStyle(sourceChild[i], targetChild[i], isDeep);
     }
   }
+}
+
+/**
+ * 复制DOM（包括样式）
+ * @param {DOM} dom
+ * @returns
+ */
+export function copyDom(dom) {
+  const newDom = dom.cloneNode(true);
+  copyStyle(dom, newDom);
+  return newDom;
 }
